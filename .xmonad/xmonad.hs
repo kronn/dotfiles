262e60b051a5685bf6057e3a8c168b6dbc9f0ace
kronn <kronn@1aa5152a-1356-48d1-b235-dfba56e26d50>
2009-07-22 08:47:15 +0000

    Xmonad-Configs hinzugefügt
    git-svn-id: http://subversion.assembla.com/svn/kronn/dotfiles@14 1aa5152a-1356-48d1-b235-dfba56e26d50

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,25 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    xmproc <- spawnPipe "/home/kronn/.cabal/bin/xmobar /home/kronn/.xmobarrc"
+    xmonad $ defaultConfig
+        { modMask = mod4Mask     -- Rebind Mod to the Windows key
+				, terminal = "xterm"
+				, manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+				-- , logHook = dynamicLog $ logHook defaultConfig
+        -- , logHook = dynamicLogDzen $ logHook defaultConfig	,
+				, logHook = dynamicLogWithPP $ xmobarPP
+								{ ppOutput = hPutStrLn xmproc
+								, ppTitle = xmobarColor "green" "" . shorten 65
+								}
+				-- , defaultGaps = [(15,0,0,0)]
+				, normalBorderColor = "#333333"
+				, focusedBorderColor = "#9999ff"
+        }
+
